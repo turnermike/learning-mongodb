7586a0b056b46d5f130c505522a2a67eaddc9cab
Mike <turner.mike@gmail.com>
2018-12-11 14:26:47 -0500

    completed Chapter3 01

--- learning_mongo/Chapter3/03_01/index.js
+++ learning_mongo/Chapter3/03_01/index.js
@@ -1,9 +1,46 @@
 // var MongoClient = require('mongodb').MongoClient;
+
+// var url = 'mongodb://localhost:27027/learning_mongo';
+
+// MongoClient.connect(url, { useNewUrlParser: true}, function(err, db) {
+//     console.log('Connected successfully!');
+//     db.close();                              // this throws an error
+// });
+
+
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27027/learning_mongo';
+// var url = 'mongodb://localhost:27017/learning_mongo';
+var url = 'mongodb://localhost:27017';
 
-MongoClient.connect(url, function(err, db) {
-    console.log('Connected successfully!');
-    db.close();
-})
+var findDocuments = function(db, callback) {
+
+    var collection = db.collection('tours');
+
+    // collection.find().toArray(function(err, docs) {
+    //     console.log(docs);
+    //     callback;
+    // })
+
+    collection.find({"tourPackage":"Snowboard Cali"}).toArray(function(err, docs) {
+        console.log(docs);
+        callback;
+    })
+}
+
+MongoClient.connect(url, {useNewUrlParser: true}, function(err, client) {
+
+    // check for connection errors
+    if (err) throw err;
+
+    // connect the db
+    var db = client.db('learning_mongo');
+    // console.log('db', db);
+
+    // look for documents and close the connection after callback
+    findDocuments(db, function(){
+        client.close();
+    });
+
+
+});
